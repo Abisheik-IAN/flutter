eba0bcece045adff37d025462e8cdc9fe7ebc00b
Abisheik-IAN <abisheganellaiyappan.I@optisolbusiness.com>
2020-12-10 17:35:45 +0530

    updated 10-12-2020

--- flutter_app/lib/aboutpage.dart
+++ flutter_app/lib/aboutpage.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
 
+import 'main.dart';
+
 
 class About extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("About"),
+        backgroundColor: Color.fromRGBO(69, 104, 220,3.0),
+      ),
+      drawer: Drawer(
+
+        child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color.fromRGBO(69, 104, 220,3.0),
+                      Color.fromRGBO(69, 104, 220,0.5),
+
+                    ],
+                  ),
+                ),
+                accountName: Text('Abisheik_cool'),
+                accountEmail: Text(' abisheganellaiyappan.I@optisolbusiness.com'),
+                currentAccountPicture: CircleAvatar(backgroundImage: NetworkImage('https://www.pinkvilla.com/files/styles/contentpreview/public/suriya_1.jpg?itok=PAyhlkot')
+                  ,)
+            ),
+            ListTile(
+              title: Text('About Page'),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (BuildContext context)=>About()));
+              },
+            ),
+            ListTile(
+              title: Text('Logout'),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (BuildContext context)=>MyApp()));
+              },
+            ),
+          ],
+        ),
+      ),
       body: Center(
         child: Text("Coming Soon"),
       ),
--- flutter_app/lib/homepage.dart
+++ flutter_app/lib/homepage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutterapp/aboutpage.dart';
 import 'package:flutterapp/main.dart';
-
+ bool _enable=false;
 class Homepage extends StatefulWidget {
 
   @override
@@ -10,18 +10,148 @@ class Homepage extends StatefulWidget {
   }
   class _Homestate extends State<Homepage>{
     int _currentIndex=0;
+    IconData _appBarIcon=null;
+
+
     final tabs=[
-      Center(child: Text('Home')),
-      Center(child: Text('Settings')),
-      Center(child: Text('My profile'))
+      Center(child: Text('Welcome what can i help you?')),
+      Center(child: Text('Coming soon')),
+      Container(
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.fromLTRB(70.0, 30.0, 80.0, 15.0),
+                  child: Center(
+                    child: CircleAvatar(
+                      backgroundImage:NetworkImage('https://www.pinkvilla.com/files/styles/contentpreview/public/suriya_1.jpg?itok=PAyhlkot') ,
+                      radius: 60,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(40.0, 5.0, 40.0, 0.0),
+                  child:Center(
+                    child: TextField(
+                      enabled:_enable,
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Name",
+                          hintStyle: TextStyle(color: Colors.grey[400])
+                      ),
+                    ),
+                  )
+                ),
+                Container(
+                    padding: EdgeInsets.fromLTRB(40.0, 5.0, 40.0, 0.0),
+                    child:Center(
+                      child: TextField(
+                        enabled: _enable,
+                        decoration: InputDecoration(
+//                          enabled: ,
+                            border: InputBorder.none,
+                            hintText: "Surname",
+                            hintStyle: TextStyle(color: Colors.grey[400])
+                        ),
+                      ),
+                    )
+                ),
+                Container(
+                    padding: EdgeInsets.fromLTRB(40.0, 5.0, 40.0, 0.0),
+                    child:Center(
+                      child: TextField(
+                        enabled: _enable,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Email",
+                            hintStyle: TextStyle(color: Colors.grey[400])
+                        ),
+                      ),
+                    )
+                ),
+                Container(
+                    padding: EdgeInsets.fromLTRB(40.0, 5.0, 40.0, 0.0),
+                    child:Center(
+                      child: TextField(
+                        enabled: _enable,
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Phone Number",
+                            hintStyle: TextStyle(color: Colors.grey[400])
+                        ),
+                      ),
+                    )
+                ),
+                Container(
+                    padding: EdgeInsets.fromLTRB(40.0, 5.0, 40.0, 0.0),
+                    child:Center(
+                      child: TextField(
+                        enabled: _enable,
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Password",
+                            hintStyle: TextStyle(color: Colors.grey[400])
+                        ),
+                      ),
+                    )
+                ),
+                RaisedButton(
+                  onPressed: () {
+
+                  },
+                  
+                  textColor: Colors.white,
+                  padding: const EdgeInsets.all(0.0),
+                  shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(50.0)),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [
+                            Color.fromRGBO(69, 104, 220,3.0),
+                            Color.fromRGBO(69, 104, 220,0.5),
+
+                          ],
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(50.0))
+                    ),
+                    padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                    child: const Text(
+                        'Submit',
+                        style: TextStyle(fontSize: 20)
+                    ),
+                  ),
+                )
+              ]
+          )
+      )
     ];
+    final app =["Home","Settings","My Profile"];
+
+
   @override
   Widget build(BuildContext context) {
+    if(_currentIndex==2){
+      _appBarIcon= Icons.edit  ;
+    }
+    else{
+      _appBarIcon=null;
+    }
     return Scaffold(
       resizeToAvoidBottomInset : false,
+
       appBar: AppBar(
-        title: Text("Home Page"),
+
+        title: Text(app[_currentIndex]),
         backgroundColor: Color.fromRGBO(69, 104, 220,3.0),
+        actions: <Widget>[
+          IconButton(icon: Icon(_appBarIcon), onPressed: (){
+            setState(() {
+              _enable=true;
+            });
+          })
+        ]
       ),
       drawer: Drawer(
 
@@ -57,7 +187,7 @@ class Homepage extends StatefulWidget {
           ],
         ),
       ),
-      body: tabs[_currentIndex],
+      body: SingleChildScrollView(child: tabs[_currentIndex]),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
 //        type: BottomNavigationBarType.fixed,
@@ -82,7 +212,8 @@ class Homepage extends StatefulWidget {
         },
       ),
     );
-  }
 
+
+  }
 
 }
